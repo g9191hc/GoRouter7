cc972327a3be85af6190d1ca483b530c44691b5a
IG <g9191hc@gmail.com>
2023-08-04 00:45:36 +0900

    Go함수

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -1,11 +1,19 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_v7_actual/screens/root_screen.dart';
 
+import '../screens/1_basic_screen.dart';
+
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => RootScreen(),
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) => BasicScreen(),
+        ),
+      ],
     ),
   ],
 );
