8260ec8ddba343e2415aa872e9b31572e6834ec1
IG <g9191hc@gmail.com>
2023-08-04 00:32:49 +0900

    프로젝트 셋팅하기

--- lib/route/router.dart
+++ lib/route/router.dart
@@ -3,6 +3,9 @@ import 'package:go_router_v7_actual/screens/root_screen.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => RootScreen()),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => RootScreen(),
+    ),
   ],
 );
